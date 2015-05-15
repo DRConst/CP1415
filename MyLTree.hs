dbf7349292aca175a2d0162f029d62ff2bc99122
drconstancio@hotmail.co <admin@legatushosting.com>
2015-05-15 12:33:00 -0400

    Finished part A

--- MyLTree.hs
+++ MyLTree.hs
@@ -1,10 +1,16 @@
 import Cp
 import LTree
---depth :: LTree c -> either a (b,b)
---depth = (either (const 1) (uncurry (max)) . (recLTree (either (const 1) max))  .  outLTree
 
---let t = Fork (Fork (Leaf 10, Fork (Leaf 2, Fork (Leaf 5, Leaf 3))), Leaf 23)
+balanced :: LTree a -> Bool
 balanced (Leaf _) = True
 balanced (Fork (t, t0)) = balanced t && balanced t0 && abs (depth t - depth t0) <=  1
+
+depth :: LTree a -> Int
 depth = cataLTree (either (const 1) (succ.(uncurry max)))
+
+balance :: LTree a -> LTree a
+balance = anaLTree lsplit . tips
+
+
+
 
--- sentinels.hs
+++ sentinels.hs
@@ -1,1 +1,32 @@
+import Cp
+
 data SList a b = Sent b | Cons (a, SList a b) deriving (Show,Eq)
+
+
+outSList :: SList a b -> Either b (a, SList a b)
+outSList (Sent b) = i1 b
+outSList (Cons( x , y )) = i2 (x, y)
+
+
+inSList :: Either b (a, SList a b) -> SList a b 
+inSList = either (Sent) (Cons)
+
+recSList f = id -|- (id >< f)
+
+cataSList f = f . (recSList (cataSList f)) . outSList
+
+anaSList f = inSList . (recSList ( anaSList f ) ) . f
+
+hyloSList f g = cataSList f . anaSList g
+
+mergeSList :: Ord a => ([a],[a]) -> [a]
+mergeSList = hyloSList (either id cons) mgen
+
+
+mgen :: Ord a => ([a], [a]) -> Either [a] (a, ([a],[a]))
+mgen (a,[]) = i1 a
+mgen ([],a) = i1 a
+mgen (x:xs, y:ys)
+	|x <= y = i2 (x, (xs, y:ys))
+	|otherwise = i2 (y, (x:xs,ys))
+
